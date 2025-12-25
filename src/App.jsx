529d5cb57b23b988c2077e35633e1599202b109e
sholdsworth <sdholdsworth86@hotmail.com>
2025-12-25 20:19:46 +0000

    updates to main app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-// import { useState } from 'react'
-import { Banner } from './components/Banner/Banner'
+import './App.css'
+import { Header } from './components/Header/Header'
 import { Navbar } from './components/Navbar/Navbar'
 import { Footer } from './components/Footer/Footer'
-import './App.css'
+import { Home } from './components/Home/Home'
 
-function App() {
+
+const App = () => {  
+
+  const configureResponsiveSiteLayout = () => {
+        const headerMyGarage = document.querySelector("#my-garage");
+        const headerLoginStatus = document.querySelector("#login-status"); 
+        const headerNavMenuBtn = document.querySelector("#nav-menu-btn");
+        const headerNavMenuBtnInput = document.querySelector("#nav-menu-btn input");
+        const navSideBar = document.querySelector("#navbar-side");
+        const navBar = document.querySelector("#navbar");
+
+        if (window.innerWidth < 640) {
+            headerMyGarage.classList.add("hidden");
+            headerLoginStatus.classList.add("hidden");
+            headerNavMenuBtn.classList.remove("hidden");
+            navBar.classList.add("hidden");
+        } else {
+            headerMyGarage.classList.remove("hidden");
+            headerLoginStatus.classList.remove("hidden");
+            headerNavMenuBtn.classList.add("hidden");
+            navBar.classList.remove("hidden");
+            navSideBar.classList.add("hidden");
+
+            if(headerNavMenuBtnInput.checked) {
+              headerNavMenuBtnInput.checked = false;
+            }
+        }
+    }
+    //determine layout on resize
+    window.onresize = () => {
+      configureResponsiveSiteLayout();
+    };
+    //determine layout on load
+    window.onload = () => {
+      configureResponsiveSiteLayout();
+    };
 
   return (
     <>
-        <Banner />
-        <Navbar />
-        <Footer />
+      <Header />
+      <Navbar />      
+      <main>
+        <Home />
+      </main>
+      <Footer />
     </>
   )
 }
