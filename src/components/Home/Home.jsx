faae876ad787a3fecc62633a64613fcdd08eb7e8
sholdsworth <sdholdsworth86@hotmail.com>
2025-12-25 20:37:58 +0000

    first main home page start

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.css'
+
+export const Home = () => {
+    return (
+        <section id="home">
+            <h1 id="main-content">Welcome to Elite Motors</h1>
+            <p className="sub-header-context">Your one-stop shop for quality vehicles and great value prices</p>
+        </section>
+    )
+}
