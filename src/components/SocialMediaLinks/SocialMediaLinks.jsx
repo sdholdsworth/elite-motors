0a39396a7c9741c229e546132a56b6ace84a4971
sholdsworth <sdholdsworth86@hotmail.com>
2026-01-17 14:27:58 +0000

    improved hover animations and updated href locations on links (to link to self as are only dummy links)

--- src/components/SocialMediaLinks/SocialMediaLinks.jsx
+++ src/components/SocialMediaLinks/SocialMediaLinks.jsx
@@ -2,19 +2,19 @@ import './SocialMediaLinks.css'
 
 export const SocialMediaLinks = () => {
     return (
-        <aside id="social-media-links" className="flex flex-row justify-between">
-            <a href="#" rel="noopener noreferrer" onClick={e => e.preventDefault}>
+        <div id="social-media-links" className="flex flex-row justify-between">
+            <a href="#facebook" rel="noopener noreferrer" onClick={e => e.preventDefault}>
                 <img src="../src/assets/social/social-facebook.svg" alt="Facebook" />
             </a>
-            <a href="#" rel="noopener noreferrer" onClick={e => e.preventDefault}>
+            <a href="#instagram" rel="noopener noreferrer" onClick={e => e.preventDefault}>
                 <img src="../src/assets/social/social-instagram.svg" alt="Instagram" />
             </a>
-            <a href="#" rel="noopener noreferrer" onClick={e => e.preventDefault}>
+            <a href="#twitter" rel="noopener noreferrer" onClick={e => e.preventDefault}>
                 <img src="../src/assets/social/social-twitter.svg" alt="Twitter" />
             </a>
-            <a href="#" rel="noopener noreferrer" onClick={e => e.preventDefault}>
+            <a href="#youtube" rel="noopener noreferrer" onClick={e => e.preventDefault}>
                 <img src="../src/assets/social/social-youtube.svg" alt="YouTube" />
             </a>
-        </aside>
+        </div>
     )   
 }
